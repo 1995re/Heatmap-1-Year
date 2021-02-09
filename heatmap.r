e9cbe4cf2dcbe72dcd24be5524c1ac953912f1ba
1995re <78517064+1995re@users.noreply.github.com>
2021-02-09 10:24:29 +0800

    Update heatmap.r

--- heatmap.r
+++ heatmap.r
@@ -2,6 +2,10 @@ library(ggplot2)
 library(ggthemes)
 library(RColorBrewer)
 
+*library(readxl)
+**data <- read_excel("data/data.xls")
+*View(data)
+
 ggplot(data, aes(Tanggal, Bulan)) +
   geom_tile(aes(fill = Jumlah)) + geom_text(aes(label=Jumlah)) +
   scale_fill_gradientn(colors = rev(brewer.pal(9, "BuPu"))) +
