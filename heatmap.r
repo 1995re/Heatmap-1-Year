efc1181ae9c973ecca76b6cf784a1b74a39bca95
1995re <syamsiralam.id@gmai.com>
2021-02-09 10:09:56 +0800

    Push to github

---
+++ heatmap.r
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(RColorBrewer)
+
+ggplot(data, aes(Tanggal, Bulan)) +
+  geom_tile(aes(fill = Jumlah)) + geom_text(aes(label=Jumlah)) +
+  scale_fill_gradientn(colors = rev(brewer.pal(9, "BuPu"))) +
+  scale_y_discrete(limits = rev(unique(data$Bulan))) +
+  ggtitle("Data Positif Harian COVID-19 (DKI Jakarta)") +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.text.x = element_text(angle = 90)) + coord_equal(ratio = 1) + theme_tufte(base_family="Helvetica") + scale_x_continuous(breaks=c(1:31), expand=c(0,0))
+
+
